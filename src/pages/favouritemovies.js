fb6ae192627b27188d09200d4b246de8a8106f00
nagaraj rathna <34264078+nagaraj28@users.noreply.github.com>
2021-09-04 22:45:33 +0530

    update:nodemodule change fr tinder react cards

--- src/pages/favouritemovies.js
+++ src/pages/favouritemovies.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "../components/navbar/navbar";
 import FavMovies from "../components/favmovies/favmovies";
+import Footer from "../components/footer/footer";
 
 export default function FavouriteMovies(){
     return (<>
